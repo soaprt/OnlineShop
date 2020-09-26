5ebb45c7ec45c78c7d6be6ff448c98a9d660c1f5
sostrovsky-edu <sergey.ostrovsky.edu@gmail.com>
2020-09-26 14:43:32 +0200

    Start project

---
+++ app/src/main/java/prt/sostrovsky/onlineshopapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package prt.sostrovsky.onlineshopapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
