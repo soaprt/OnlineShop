7d6fb528024a6432424aebd84dbc88cff24d1070
sostrovsky-prt <sergey.ostrovsky.prt@gmail.com>
2020-09-28 10:13:49 +0200

    Add custom toolbar

--- app/src/main/java/prt/sostrovsky/onlineshopapp/ui/MainActivity.kt
+++ app/src/main/java/prt/sostrovsky/onlineshopapp/ui/MainActivity.kt
@@ -1,64 +1,24 @@
 package prt.sostrovsky.onlineshopapp.ui
 
 import android.os.Bundle
-import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
-import prt.sostrovsky.onlineshopapp.R
-
-class MainActivity : BaseActivity(R.id.rootLayout) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setNavController()
-    }
-
-    private fun setNavController() {
-        navController = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
-    }
-}
-
-/*
-import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
 import prt.sostrovsky.onlineshopapp.R
 
 class MainActivity : BaseActivity(R.id.rootLayout) {
-//    private lateinit var appBarConfiguration: AppBarConfiguration
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        val navHostFragment =
-//            supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
-//        val navController = navHostFragment.navController
-//        appBarConfiguration = AppBarConfiguration(navController.graph)
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-
         setNavController()
-        setAppBar()
+        setToolBar()
     }
 
     private fun setNavController() {
         navController = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
-    private fun setAppBar() {
-//        val appBarConfiguration = AppBarConfiguration(navController.graph)
-//        findViewById<Toolbar>(R.id.toolbar)
-//            .setupWithNavController(navController, appBarConfiguration)
-
-//        supportActionBar?.setLogo(R.drawable.app_logo)
-//        supportActionBar?.setDisplayUseLogoEnabled(true)
+    private fun setToolBar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
-}*/
+}
